5abf7aed32070720961c01ed73bfebb91fd84862
Nekuskus <contact@kuskus.dev>
2024-12-06 15:05:27 +0100

    Solved 06!

---
+++ src/06.rs
@@ -0,0 +1,245 @@
+#![feature(iter_repeat_n)]
+
+use debug_print::{debug_print as debug, debug_println as debugln};
+use setup_utils::*;
+use std::{borrow::BorrowMut, collections::HashSet, iter, path::Path};
+
+// Symbols to replace: 06 41 6 4977 SOLVE2
+
+#[cfg(test)]
+mod tests {
+    use setup_utils::read_lines;
+    use std::path::Path;
+
+    #[test]
+    fn part1() -> Result<(), String> {
+        let lines = read_lines(Path::new("./inputs/06-example.txt"));
+        let result = crate::part1(&lines);
+        if result == 41 {
+            Ok(())
+        } else {
+            Err(format!(
+                "06: Bad result for Part 1 example, expected 41 got {}",
+                result
+            ))
+        }
+    }
+
+    #[test]
+    fn part2() -> Result<(), String> {
+        let lines = read_lines(Path::new("./inputs/06-example.txt"));
+        let result = crate::part2(&lines);
+        if result == 6 {
+            Ok(())
+        } else {
+            Err(format!(
+                "06: Bad result for Part 2 example, expected 6 got {}",
+                result
+            ))
+        }
+    }
+
+    #[test]
+    fn full() -> Result<(), String> {
+        let lines = read_lines(Path::new("./inputs/06-full.txt"));
+        let result1 = crate::part1(&lines);
+        //let result2 = crate::part2(&lines);
+
+        if result1 == 4977 {
+            Ok(())
+        } else {
+            Err(format!(
+                "06: Bad result for Part 1, expected 4977 got {}",
+                result1
+            ))
+        }
+        /*
+        match (result1, result2) {
+            (4977, SOLVE2) => Ok(()),
+            (_, SOLVE2) => Err(format!(
+                "06: Bad result for Part 1, expected 4977 got {}",
+                result1
+            )),
+            (4977, _) => Err(format!(
+                "06: Bad result for Part 2, expected SOLVE2 got {}",
+                result2
+            )),
+            (_, _) => Err(format!(
+                "06: Bad result for Part 1 & 2, expected (4977, SOLVE2) got ({}, {})",
+                result1, result2
+            )),
+        }*/
+    }
+}
+
+fn main() {
+    let linesfull = read_lines(Path::new("./inputs/06-full.txt"));
+    let lines1 = read_lines(Path::new("./inputs/06-example.txt"));
+
+    println!("06-full.txt");
+    println!("{}", part1(&linesfull));
+    println!("{}\n", part2(&linesfull));
+
+    println!("06-1-example.txt");
+    println!("{}", part1(&lines1));
+    println!("{}", part2(&lines1));
+}
+
+fn matrix_step(matrix: &Vec<Vec<char>>, p: &Point, d: Direction) -> Option<(Point, Direction)> {
+    let xlen = matrix[0].len();
+    let ylen = matrix.len();
+
+    match d {
+        Direction::North => {
+            if p.y > 0 {
+                if matrix[p.y - 1][p.x] != '#' {
+                    Some((Point { x: p.x, y: p.y - 1 }, Direction::North))
+                } else {
+                    matrix_step(matrix, p, Direction::East)
+                }
+            } else {
+                None
+            }
+        }
+        Direction::East => {
+            if p.x < xlen - 1 {
+                if matrix[p.y][p.x + 1] != '#' {
+                    Some((Point { x: p.x + 1, y: p.y }, Direction::East))
+                } else {
+                    matrix_step(matrix, p, Direction::South)
+                }
+            } else {
+                None
+            }
+        }
+        Direction::South => {
+            if p.y < ylen - 1 {
+                if matrix[p.y + 1][p.x] != '#' {
+                    Some((Point { x: p.x, y: p.y + 1 }, Direction::South))
+                } else {
+                    matrix_step(matrix, p, Direction::West)
+                }
+            } else {
+                None
+            }
+        }
+        Direction::West => {
+            if p.x > 0 {
+                if matrix[p.y][p.x - 1] != '#' {
+                    Some((Point { x: p.x - 1, y: p.y }, Direction::West))
+                } else {
+                    matrix_step(matrix, p, Direction::North)
+                }
+            } else {
+                None
+            }
+        }
+    }
+}
+
+fn part1(lines: &Vec<String>) -> usize {
+    let mut matrix = lines
+        .iter()
+        .map(|s| s.chars().collect())
+        .collect::<Vec<Vec<char>>>();
+    let mut startx = None;
+    let mut starty = None;
+    'search: for (y, line) in matrix.iter().enumerate() {
+        for (x, c) in line.iter().enumerate() {
+            if *c == '^' {
+                startx = Some(x);
+                starty = Some(y);
+                break 'search;
+            }
+        }
+    }
+
+    assert!(startx.is_some() && starty.is_some());
+
+    let (mut p, mut d) = (
+        Point {
+            x: startx.unwrap(),
+            y: starty.unwrap(),
+        },
+        Direction::North,
+    );
+
+    matrix[p.y][p.x] = 'X';
+
+    while let Some((newp, newd)) = matrix_step(&matrix, &p, d) {
+        p = newp;
+        d = newd;
+        matrix[p.y][p.x] = 'X';
+    }
+
+    for line in matrix.clone() {
+        debugln!("{line:?}");
+    }
+
+    matrix
+        .iter()
+        .map(|line| line.iter().filter(|&&c| c == 'X').count())
+        .sum()
+}
+
+fn part2(lines: &Vec<String>) -> usize {
+    let mut matrix = lines
+        .iter()
+        .map(|s| s.chars().collect())
+        .collect::<Vec<Vec<char>>>();
+    let mut startx = None;
+    let mut starty = None;
+    'search: for (y, line) in matrix.iter().enumerate() {
+        for (x, c) in line.iter().enumerate() {
+            if *c == '^' {
+                startx = Some(x);
+                starty = Some(y);
+                break 'search;
+            }
+        }
+    }
+
+    assert!(startx.is_some() && starty.is_some());
+
+    let (p, d) = (
+        Point {
+            x: startx.unwrap(),
+            y: starty.unwrap(),
+        },
+        Direction::North,
+    );
+
+    matrix[p.y][p.x] = 'X';
+
+    iter::repeat_n(&matrix, matrix.len() * matrix[0].len())
+        .enumerate()
+        .filter(|(idx, matrix_ref)| {
+            let mut matrix = (*matrix_ref).clone();
+            let (x, y) = (idx % matrix[0].len(), idx / matrix.len());
+            if matrix[y][x] == 'X' {
+                // don't replace starting position
+                return false;
+            }
+
+            matrix[y][x] = '#';
+
+            let (mut p, mut d) = (p.clone(), d.clone());
+
+            let mut history = HashSet::new();
+            history.insert((p, d));
+
+            while let Some((newp, newd)) = matrix_step(&matrix, &p, d) {
+                p = newp;
+                d = newd;
+                matrix[p.y][p.x] = 'X';
+                if history.contains(&(p, d)) {
+                    // loop found
+                    return true;
+                }
+                history.insert((p, d));
+            }
+
+            false
+        })
+        .count()
+}
--- src/setup_utils.rs
+++ src/setup_utils.rs
@@ -2,20 +2,43 @@ use std::fs::read_to_string;
 use std::path::Path;
 
 pub fn read_lines(filename: &Path) -> Vec<String> {
-    read_to_string(filename) 
-        .unwrap()  // panic on possible file-reading errors
-        .lines()  // split the string into an iterator of string slices
+    read_to_string(filename)
+        .unwrap() // panic on possible file-reading errors
+        .lines() // split the string into an iterator of string slices
         .map(|s| s.trim().to_owned())
-        .collect()  // gather them together into a vector
+        .collect() // gather them together into a vector
 }
 
-pub fn pad_lines(lines: &Vec<String>) -> Vec<String> {
-    let mut new_vec = lines.clone();
-    for line in new_vec.iter_mut() {
-        line.insert_str(0, ".");
-        line.insert_str(line.len(), ".");
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+pub enum Direction {
+    North,
+    South,
+    West,
+    East,
+}
+
+impl Direction {
+    pub fn rotate_right(&self) -> Self {
+        match self {
+            Self::North => Self::East,
+            Self::East => Self::South,
+            Self::South => Self::West,
+            Self::West => Self::North,
+        }
     }
-    new_vec.insert(0, ".".repeat(new_vec[0].len()));
-    new_vec.insert(new_vec.len(), ".".repeat(new_vec[0].len()));
-    return new_vec;
+
+    pub fn rotate_left(&self) -> Self {
+        match self {
+            Self::North => Self::West,
+            Self::West => Self::South,
+            Self::South => Self::East,
+            Self::East => Self::North,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
+pub struct Point {
+    pub x: usize,
+    pub y: usize,
 }
