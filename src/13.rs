951b5bc4e2fc64acb348b4ce03f47401473b642d
Nekuskus <contact@kuskus.dev>
2025-07-10 15:17:57 +0200

    Saving early for 13.2, TODO left in comment for final solution.

--- src/13.rs
+++ src/13.rs
@@ -2,7 +2,7 @@ use setup_utils::*;
 use std::path::Path;
 use debug_print::{debug_print as debug, debug_println as debugln};
 
-// Symbols to replace: 13 405 TEST2 SOLVE1 SOLVE2
+// Symbols to replace: 13 405 400 30802 SOLVE2
 
 
 #[cfg(test)]
@@ -20,15 +20,15 @@ mod tests {
             Err(format!("13: Bad result for Part 1 example, expected 405 got {}", result))
         }
     }
-    /*
+    
     #[test]
     fn part2() -> Result<(), String> {
-        let lines = read_lines(Path::new("./inputs/13-2-example.txt"));
+        let lines = read_lines(Path::new("./inputs/13-example.txt"));
         let result = crate::part2(&lines);
-        if result == TEST2 {
+        if result == 400 {
             Ok(())
         } else {
-            Err(format!("13: Bad result for Part 2 example, expected TEST2 got {}", result))
+            Err(format!("13: Bad result for Part 2 example, expected 400 got {}", result))
         }
     }
 
@@ -38,39 +38,32 @@ mod tests {
         let result1 = crate::part1(&lines);
         //let result2 = crate::part2(&lines);
         
-        if result1 == SOLVE1 {
+        if result1 == 30802 {
             Ok(())
         } else {
-            Err(format!("13: Bad result for Part 1, expected SOLVE1 got {}", result1))
+            Err(format!("13: Bad result for Part 1, expected 30802 got {}", result1))
         }
         /*
         match (result1, result2) {
-            (SOLVE1, SOLVE2) => Ok(()),
-            (_, SOLVE2) => Err(format!("13: Bad result for Part 1, expected SOLVE1 got {}", result1)),
-            (SOLVE1, _) => Err(format!("13: Bad result for Part 2, expected SOLVE2 got {}", result2)),
-            (_, _) => Err(format!("13: Bad result for Part 1 & 2, expected (SOLVE1, SOLVE2) got ({}, {})", result1, result2))
+            (30802, SOLVE2) => Ok(()),
+            (_, SOLVE2) => Err(format!("13: Bad result for Part 1, expected 30802 got {}", result1)),
+            (30802, _) => Err(format!("13: Bad result for Part 2, expected SOLVE2 got {}", result2)),
+            (_, _) => Err(format!("13: Bad result for Part 1 & 2, expected (30802, SOLVE2) got ({}, {})", result1, result2))
         }*/
     }
-    */
 }
 
 fn main() {
     let linesfull = read_lines(Path::new("./inputs/13-full.txt"));
     let lines1 = read_lines(Path::new("./inputs/13-example.txt"));
-    //let lines2 = read_lines(Path::new("./inputs/13-2-example.txt"));
 
     println!("13-full.txt");
     println!("{}", part1(&linesfull));
-    //println!("{}\n", part2(&linesfull));
+    println!("{}\n", part2(&linesfull));
     
     println!("13-1-example.txt");
     println!("{}", part1(&lines1));
-    //println!("{}\n", part2(&lines1));
-    
-    
-    //println!("13-2-example.txt");
-    //println!("{}", part1(&lines2));
-    //println!("{}", part2(&lines2));
+    println!("{}\n", part2(&lines1));
     
 }
 
@@ -133,8 +126,102 @@ fn part1(lines: &Vec<String>) -> i32 {
 
     cumsum as i32
 }
-/*
-fn part2(lines: &Vec<String>) -> i32 {
 
+fn part2(lines: &Vec<String>) -> i32 {
+    let sections = lines.split(|line| line == "").map(|arr| arr.iter().map(|line| line.chars().collect::<Vec<char>>()).collect::<Vec<Vec<_>>>()).collect::<Vec<Vec<Vec<char>>>>();
+    let mut cumsum = 0;
+
+    let mut i = 0;
+    for section in sections {
+        let mut found_smudge = false;
+        let mut found_mirror = false;
+        for y in 0..section.len() - 1 {
+            if section[y] == section[y + 1] { // TODO: REDO THIS CHECK TO ALLOW SMUDGES, WILL WORK FINE AFTERWARDS
+                let diff_from_line = match () {
+                    _ if y + 1 > section.len() / 2 => section.len() - (y + 2),
+                    _ => y// if y <= section.len() / 2
+                };
+
+                if (1..=diff_from_line).into_iter().all(|offset| {
+                    let both_sides: Vec<(char, char)> = section[y-offset].iter().zip(section[y+1+offset].iter()).map(|(&left, &right)| (left, right)).collect();
+                    let inequalities = both_sides.iter().fold(0, |acc, (left, right)| acc + if left == right { 0 } else { 1 });    
+                    if inequalities == 0 {
+                        true
+                    } else if inequalities == 1 {
+                        if !found_smudge {
+                            found_smudge = true;
+                            true
+                        } else {
+                            false
+                        }
+                    } else {
+                        false
+                    }
+                }) {
+                    if found_smudge {
+                        found_mirror = true;
+                        debugln!("Section {i}, found at y: {}|{}", y, y+1);
+                        debugln!("diff: {diff_from_line}");
+                        cumsum += (y + 1) * 100;
+                        break;
+                    }
+                }
+            } else {
+                if found_smudge {
+                    found_smudge = false;
+                }
+            }
+        }
+        if !found_mirror {
+            for x in 0..section[0].len() - 1 {
+                if section.iter().all(|vec_char| vec_char[x] == vec_char[x+1]) { // TODO: REDO THIS CHECK TO ALLOW SMUDGES, WILL WORK FINE AFTERWARDS
+                    let diff_from_line = match () {
+                        _ if x + 1 > section[0].len() / 2 => section[0].len() - (x + 2),
+                        _ => x// if y <= section.len() / 2
+                    };
+                    debugln!("diff_before_check: {diff_from_line}");
+                    for line in &section {
+                        debugln!("{}", line.iter().collect::<String>());
+                    }
+                    if (1..=diff_from_line).into_iter().all(|offset| {
+                        debugln!("Checking offset {offset}, {} == {} for x: {}|{}", x-offset, x+1+offset, x, x+1);
+                        for line in &section {
+                            debugln!("{} == {}", line[x-offset], line[x+1+offset]);
+                        }
+                        let both_sides: Vec<(char, char)> = section.iter().map(|vec_char| (vec_char[x-offset], vec_char[x+1+offset])).collect();
+                        let inequalities = both_sides.iter().fold(0, |acc, (left, right)| acc + if left == right { 0 } else { 1 });
+                        if inequalities == 0 {
+                            true
+                        } else if inequalities == 1 {
+                            if !found_smudge {
+                                found_smudge = true;
+                                true
+                            } else {
+                                false
+                            }
+                        } else {
+                            false
+                        }
+                    }) {
+                        if found_smudge {
+                            found_mirror = true;
+                            debugln!("Section {i}, found at x: {}|{}", x+1, x+2);
+                            debugln!("diff: {diff_from_line}");
+                            cumsum += (x + 1);
+                            break;   
+                        }
+                    } else {
+                        if found_smudge {
+                            found_smudge = false;
+                        }
+                    }
+                }
+            }
+        }
+        assert_eq!(found_mirror, true);
+        assert_eq!(found_smudge, true);
+        i += 1;
+    }
+
+    cumsum as i32
 }
-*/
